9d414fe6e5da62677e97e9bfaff45fd8011f021c
adamkraus6 <adamkraus6@gmail.com>
2020-12-07 14:16:07 -0600

    Added max.h header

---
+++ max.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream ts(s);
+    while (std::getline(ts, token, delimiter)) tokens.push_back(token);
+    return tokens;
+}
