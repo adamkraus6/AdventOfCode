f62b9cbb69d329329d40e839c4676556fb9d6bfb
adamkraus6 <adamkraus6@gmail.com>
2020-12-15 17:09:49 -0600

    2020 day 13 part 2 incomplete - Chinese Remainder Theorem Issues

--- 2020/2020day13/2020day13.cpp
+++ 2020/2020day13/2020day13.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int solveFirstDeparture(vector<int> IDs, int timestamp);
+long long magicTimestamp(vector<string> buses);
+long long modInv(int a, int m);
 
 int main()
 {
@@ -13,7 +15,7 @@ int main()
 	if(!fin.is_open()) exit(0);
 
 	string line;
-	int timestamp, firstDepart;
+	int timestamp;
 
 	getline(fin, line);
 	timestamp = stoi(line);
@@ -33,6 +35,8 @@ int main()
 	}
 
 	cout << "Product of minute wait and Bus ID for first departure" << endl << solveFirstDeparture(busIDs, timestamp) << endl;
+
+	cout << "Magic timestamp for departures" << endl << magicTimestamp(buses) << endl;
 }
 
 int solveFirstDeparture(vector<int> IDs, int timestamp)
@@ -53,8 +57,79 @@ int solveFirstDeparture(vector<int> IDs, int timestamp)
 	return (firstDepart - timestamp) * busID;
 }
 
-/*
-Part 2 Answer: 530015546283687 (NOT FROM MY SOLUTION)
-https://github.com/zv0n/advent_of_code_2020/blob/master/13/main.cpp
-Look into Chinese Remainder Theorem
-*/
+long long magicTimestamp(vector<string> buses)
+{
+	long long t;
+	vector<long long> num, rem, pp, inv;
+
+	// calculate offset
+	for (int i = 0; i < (int)buses.size(); i++)
+	{
+		if (buses[i] != "x")
+		{
+			int id = stoi(buses[i]);
+			num.push_back(id);
+			int offset = id - i;
+			while (offset < 0)
+			{
+				offset += id;
+			}
+			rem.push_back(offset);
+		}
+	}
+
+	long long prod = 1;
+	for (int i = 0; i < (int)num.size(); i++)
+	{
+		prod *= num[i];
+
+		pp.push_back(prod / num[i]);
+
+		inv.push_back(modInv(pp[i], num[i]));
+	}
+
+	unsigned long long sum = 0;
+	for (int i = 0; i < (int)rem.size(); i++)
+	{
+		sum += (rem[i] * pp[i] * inv[i]);
+	}
+
+	t = sum % prod;
+
+	return t;
+}
+
+// from https://www.geeksforgeeks.org/chinese-remainder-theorem-set-2-implementation/
+long long modInv(int a, int m)
+{
+	long long m0 = m, t, q;
+	long long x0 = 0, x1 = 1;
+
+	if (m == 1)
+		return 0;
+
+	// Apply extended Euclid Algorithm 
+	while (a > 1)
+	{
+		// q is quotient 
+		q = a / m;
+
+		t = m;
+
+		// m is remainder now, process same as 
+		// euclid's algo 
+		m = a % m, a = t;
+
+		t = x0;
+
+		x0 = x1 - q * x0;
+
+		x1 = t;
+	}
+
+	// Make x1 positive 
+	if (x1 < 0)
+		x1 += m0;
+
+	return x1;
+}
